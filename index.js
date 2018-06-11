7e647992df1c54924d938d63fa3e2c345f12ccbd
Nina Sweeney <nsweeney@middlebury.edu>
2018-06-11 22:27:55 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,22 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  var spy=string
+  console.log(string.toLowerCase);
+}
+
+// logWhisper
+
+function sayHiToGrandma(string) {
+  var lowercase= "hello"
+  if (string.toLowerCase === lowercase){
+    answer= "I can't hear you!";
+  }
+  return (answer);
+}
